7ada01170cb864e405ca9629f476b6d94d2c78be
Andras Dobreff <dobreffandras@gmail.com>
2023-06-20 16:13:30 +0200

    Initial Commit

---
+++ BlazCommerce/Server/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazCommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return Ok(new[]
+            {
+                new Product(){ Id = 1, Name = "Product1" },
+                new Product(){ Id = 2, Name = "Product2" },
+                new Product(){ Id = 3, Name = "Product3" },
+                new Product(){ Id = 4, Name = "Product4" },
+            });
+        }
+    }
+}
