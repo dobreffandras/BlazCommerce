cad32b7f957f35b25ded44d76fcae0bb3a43314d
Andras Dobreff <dobreffandras@gmail.com>
2023-06-23 18:22:08 +0200

    Introduce ProductsService on the client

---
+++ BlazCommerce/Client/Services/ProductsService.cs
@@ -0,0 +1,23 @@
+﻿using BlazCommerce.Shared;
+using System.Net.Http.Json;
+
+namespace BlazCommerce.Client.Services;
+
+public class ProductsService
+{
+    private readonly HttpClient httpClient;
+
+    public ProductsService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+    public Task<Product[]> GetProducts()
+    {
+        return httpClient.GetFromJsonAsync<Product[]>("api/products");
+    }
+
+    public async Task CreateProduct(NewProduct newProduct)
+    {
+        await httpClient.PostAsJsonAsync("api/products", newProduct);
+    }
+}
